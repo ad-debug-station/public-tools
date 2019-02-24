b7be87b64eef602864f2eea3271afa408ce280c9
えむ a.k.a. STSynthe <sts2657+github@gmail.com>
2019-02-25 08:56:44 +0900

    Update update-bt.sh

--- update-bt.sh
+++ update-bt.sh
@@ -7,45 +7,73 @@
 # 
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+#===================================#
+# Update script for BuildTools v1.2 #
+#===================================#
+
 cd `dirname $0`
 
-BT_NAME="BuildTools"
-BT_NUMBER_FILENAME="${BT_NAME}.number"
-BT_APP_FILENAME="${BT_NAME}.jar"
 REV="1.13.2"
-#SOURCE="${PWD}/spigot-${REV}.jar"
 MAVEN_OPTS="-Xmx2G -Xms1G -XX:+UseG1GC"
+START_BUILDTOOLS=1
 
-if [ ! -e $BT_NUMBER_FILENAME ]; then
-	echo 0 > $BT_NUMBER_FILENAME
-	chmod 0600 $BT_NUMBER_FILENAME
+BT_NAME="BuildTools"
+BT_JARFILE="${BT_NAME}.jar"
+BT_NUMFILE="${BT_NAME}.number"
+LOCAL_NUMBER=0
+CURRENT_NUMBER=$(curl --silent https://hub.spigotmc.org/jenkins/job/BuildTools/lastStableBuild/buildNumber)
+
+if [ -e $BT_NUMFILE ]; then
+	LOCAL_NUMBER=$(cat $BT_NUMFILE)
 fi
 
-LOCAL_NUMBER=$(cat $BT_NUMBER_FILENAME)
-CURRENT_NUMBER=$(curl --silent https://hub.spigotmc.org/jenkins/job/BuildTools/lastStableBuild/api/json | jq '.number')
+SG_REPO="craftbukkit"
+LOCAL_CID_FILE="${SG_REPO}.commitid"
+LOCAL_CID="0"
+LAST_CID=$(curl --silent https://hub.spigotmc.org/stash/rest/api/1.0/projects/SPIGOT/repos/${SG_REPO}/commits?limit=1 | jq -r '.values[0].id')
+
+if [ -e $LOCAL_CID_FILE ]; then
+	LOCAL_CID=$(cat $LOCAL_CID_FILE)
+fi
+
+EXTEND_SCRIPT="extend-update-bt.sh"
+
+function event_log {
+	local PID=${2}
+	if [ ! ${2} ]; then
+		PID=$$
+	fi
+
+	logger --id $PID --tag update-bt "${1}"
+	return $?
+}
+
+event_log "I run it. [LOCAL_NUMBER = ${LOCAL_NUMBER}, CURRENT_NUMBER = ${CURRENT_NUMBER}]"
 
 if [ $LOCAL_NUMBER -lt $CURRENT_NUMBER ]; then
-	echo "BuildTools Updating..."
-	echo $CURRENT_NUMBER > $BT_NUMBER_FILENAME
-	mv ${BT_APP_FILENAME} ${BT_APP_FILENAME}.old
-	curl --silent -o $BT_APP_FILENAME https://hub.spigotmc.org/jenkins/job/BuildTools/lastStableBuild/artifact/target/BuildTools.jar
+	event_log "Need to update BuildTools"
+	echo ${CURRENT_NUMBER} > ${BT_NUMFILE}
+	if [ -e $BT_JARFILE ]; then
+		mv ${BT_JARFILE} ${BT_JARFILE}.old
+	fi
+	curl --silent -o ${BT_JARFILE} https://hub.spigotmc.org/jenkins/job/BuildTools/lastStableBuild/artifact/target/BuildTools.jar &
+	PID=$!
+	wait $PID
+	event_log "Finish update BuildTools. Status: $?" $PID
 fi
 
-SPIGOTMC_REPO="craftbukkit"
-LOCAL_COMMIT_ID_FILENAME="${SPIGOTMC_REPO}.commitid"
-if [ ! -e $LOCAL_COMMIT_ID_FILENAME ]; then
-	echo 0 > $LOCAL_COMMIT_ID_FILENAME
-	chmod 0600 $LOCAL_COMMIT_ID_FILENAME
+if [ $START_BUILDTOOLS -a $LOCAL_CID != $LAST_CID ]; then
+	event_log "Need to update Bukkit/Spigot. Start BuildTools!"
+	echo ${LAST_CID} > ${LOCAL_CID_FILE}
+	java -d64 ${MAVEN_OPTS} -jar ${BT_JARFILE} --rev ${REV} &
+	PID=$!
+	wait $PID
+	event_log "Finish build. Status: $?" $PID
+	if [ -x $EXTEND_SCRIPT ]; then
+		source ${EXTEND_SCRIPT}
+		event_log "Extended script was executed. Status: $?"
+	fi
 fi
 
-LOCAL_COMMIT_ID=$(cat $LOCAL_COMMIT_ID_FILENAME)
-LAST_COMMIT_ID=$(curl --silent https://hub.spigotmc.org/stash/rest/api/1.0/projects/SPIGOT/repos/${SPIGOTMC_REPO}/commits?limit=2 | jq -r '.values[0].id')
-
-if [ $LOCAL_COMMIT_ID != $LAST_COMMIT_ID ]; then
-	echo "need update..."
-	echo $LAST_COMMIT_ID > $LOCAL_COMMIT_ID_FILENAME
-	java -d64 -Xmx2G -Xms1G -XX:+UseG1GC -jar $BT_APP_FILENAME --rev $REV
-fi
-
-echo "Good bye!!"
-exit
+event_log "I end."
+exit 0
